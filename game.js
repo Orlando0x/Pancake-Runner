ca837755e35bd62554d7f77931c1a98e06a4bcde
Orlando0x <rodor29@gmail.com>
2022-08-31 20:52:48 -0400

    creando array bidimensional para los mapas

--- game.js
+++ game.js
@@ -8,23 +8,22 @@ window.addEventListener('load', starGame);
 window.addEventListener('resize', starGame);
 
 function starGame(){
-
     canvasResize()
+    game.textAlign = 'start';
     game.font = elementSize + 'px Arial'
     for (let i = 0; i < 10; i++) {
-        game.fillText('💣', (elementSize*i -10) , elementSize)
-    }
-    
-}
-    function canvasResize (){
-    
-        if(window.innerWidth < window.innerHeight){
-            canvasSize = window.innerWidth * 0.70
-        } else {
-            canvasSize = window.innerHeight * 0.70;
+        for (let z = 1; z <= 10; z++) {
+            game.fillText(emojis[mapRowsCols[2][i][z-1]], elementSize*i-10, elementSize*z-5)
         }
+    }
+}
+function canvasResize (){
+    if(window.innerWidth < window.innerHeight){
+        canvasSize = window.innerWidth * 0.70;
+    } else {
+        canvasSize = window.innerHeight * 0.70;
+    }
     canvas.setAttribute('width', canvasSize );
     canvas.setAttribute('height', canvasSize );
     elementSize = canvasSize / 10;
-
 }
